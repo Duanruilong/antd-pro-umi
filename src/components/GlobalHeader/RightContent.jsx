734f0670a0828487ea0163b36e0926951441791a
duanruilong <drl1210@163.com>
2019-12-23 14:16:42 +0800

    去除top栏搜索-HeaderSearch

--- src/components/GlobalHeader/RightContent.jsx
+++ src/components/GlobalHeader/RightContent.jsx
@@ -1,8 +1,7 @@
-import { Icon, Tooltip } from "antd";
+import { Icon } from "antd";
 import React from "react";
 import { connect } from "dva";
 import Avatar from "./AvatarDropdown";
-import HeaderSearch from "../HeaderSearch";
 import styles from "./index.less";
 
 const GlobalHeaderRight = props => {
@@ -15,28 +14,6 @@ const GlobalHeaderRight = props => {
 
   return (
     <div className={className}>
-      <HeaderSearch
-        className={`${styles.action} ${styles.search}`}
-        placeholder="站内搜索"
-        defaultValue="umi ui"
-        dataSource={["搜索提示一", "搜索提示二", "搜索提示三"]}
-        onSearch={value => {
-          console.log("input", value);
-        }}
-        onPressEnter={value => {
-          console.log("enter", value);
-        }}
-      />
-      <Tooltip title="使用文档">
-        <a
-          target="_blank"
-          href="https://pro.ant.design/docs/getting-started"
-          rel="noopener noreferrer"
-          className={styles.action}
-        >
-          <Icon type="question-circle-o" />
-        </a>
-      </Tooltip>
       <Avatar />
     </div>
   );
